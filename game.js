3c44a82691b257902e93818a0a3e4d95366b6d56
dkhayutin <dan.khayutin@gmail.com>
2025-04-29 18:45:20 -0400

    function that returns string of rock, paper, or scissor

---
+++ game.js
@@ -0,0 +1,14 @@
+function getRandom(num){
+  let string = Math.floor(Math.random() * num)
+   if(string == 0) {
+    return 'rock'
+   } else if (string == 1){
+    return 'paper'
+   } else {
+    return 'scissors'
+   }
+  
+}
+
+console.log(getRandom(3))
+
