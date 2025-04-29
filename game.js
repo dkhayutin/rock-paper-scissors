efdc96174489bd7dbe1b4bd6232ea05404730bcf
dkhayutin <dan.khayutin@gmail.com>
2025-04-29 19:34:38 -0400

    created prompt input for 'rock papper scissor human side

--- game.js
+++ game.js
@@ -1,3 +1,6 @@
+let humanScore = 0;
+let computerScore = 0;
+
 function getComputerChoice(num){
   let string = Math.floor(Math.random() * num)
    if(string == 0) {
@@ -12,3 +15,18 @@ function getComputerChoice(num){
 
 console.log(getComputerChoice(3))
 
+function getHumanChoice(){
+  let string = prompt('Choose your weapon')
+   if(string.toLowerCase() === 'rock'){
+    console.log(string)
+   } else if (string.toLowerCase() ==='paper'){
+    console.log(string)
+   } else if (string.toLowerCase() ==='scissors'){
+    console.log(string)
+   } else {
+    console.log('Invalid input')
+   }
+  }
+
+
+getHumanChoice()
